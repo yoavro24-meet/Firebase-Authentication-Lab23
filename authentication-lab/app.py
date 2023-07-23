24d05475b5157f87b6600b30226be41fe90319d5
yoavro24-meet <139694379+yoavro24-meet@users.noreply.github.com>
2023-07-23 12:13:08 +0300

    s
    s

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -8,13 +8,34 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == "POST"
+        email = request.form['email']
+        passsword = request.form['password']
+        try:
+            login_session['user'] = 
+            auth.create_user_with_email_and_password(email,password)
+            return redirect(url_for("add_tweet"))
+        except:
+            error = "sorry u didnt succeed"
+            return render_template("signin.html")
+
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
-
+    error = ""
+    if request.method == "POST"
+        email = request.form['email']
+        passsword = request.form['password']
+        try:
+            login_session['user'] = 
+            auth.create_user_with_email_and_password(email,password)
+            return redirect(url_for("add_tweet"))
+        except:
+            error = "sorry u didnt succeed"
+            return render_template("signup.html")
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
